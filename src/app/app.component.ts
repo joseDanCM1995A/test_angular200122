f84cf4d867688204117507df64b83ca9de6563db
José Daniel Contreras Medina <jcontreras@onikom.com.mx>
2022-01-20 18:54:16 -0600

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'test1';
+export class AppComponent implements OnInit {
+  //declaracion de variables
+  name !: string;
+  lastname !: string
+  obj: Object
+  show: boolean
+  constructor() { //inicializando variables
+    this.name = ""
+    this.lastname = ""
+    this.obj = {
+      name: this.name,
+      lastname: this.lastname
+    }
+    this.show = false
+  }
+  ngOnInit() { 
+
+  }
+  showObject() {
+    this.obj = {  //asignando valores al objeto
+      name: this.name,
+      lastname: this.lastname
+    }
+    this.obj = JSON.stringify(this.obj) //para que se muestre bien en pantalla se transforma a string
+    this.show = true //para mostrar el objetyo una vez que se pulse el boton
+  }
+
 }
